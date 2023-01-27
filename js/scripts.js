fb717aa07cacd3a9e6ce04b7858792dc5b5de31b
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-27 15:24:12 -0800

    update some HTML while I give myself a break from functions to see if I can come back at them fresh in a bit.

--- js/scripts.js
+++ js/scripts.js
@@ -52,69 +52,52 @@ function orderSummary() {
 let niceSlice = new NiceSlice();
 
 function orderSummary(orderSummaryToDisplay) {
-  let contactsDiv = document.querySelector("div#pizzaTotal");
-  contactsDiv.innerText =  null;
-  const ul = document.createElement("ul");
-  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
-    const contact = addressBookToDisplay.findContact(key);
-    const li = document.createElement("li");
-    li.append(contact.fullName());
-    li.setAttribute("id", contact.id);
-    ul.append(li);
-  });
-  contactsDiv.append(ul);
+  document.querySelector(".first-name").innerText = contact.firstName;
+  let pizzaTotalDiv = document.querySelector("div#pizzaTotal");
+  return 
 }
 
 window.addEventListener("load", function() {
 
 })
 
-// adding this function to keep up separation of concerns. This will make a UI list function to display the objects we've taken in.
-function listContacts(addressBookToDisplay) {
-  let contactsDiv = document.querySelector("div#contacts");
-  contactsDiv.innerText =  null;
-  const ul = document.createElement("ul");
-  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
-    const contact = addressBookToDisplay.findContact(key);
-    const li = document.createElement("li");
-    li.append(contact.fullName());
-    li.setAttribute("id", contact.id);
-    ul.append(li);
-  });
-  contactsDiv.append(ul);
-}
-// example: contactsDiv.append(ul) is our single hit to the DOM *which is most EFFICIENT* as opposed to having it say something like: contactsList.append(li)
-
-// this function will allow clicking on the name to show information in the console
-function displayContactDetails(event) {
-  const contact = addressBook.findContact(event.target.id); //adding our .findContact()method from our Business Logic we're cheating a bit b/c addressBook is global, which is standing in for a database
-  // .class-name #id-name . calls from class-name, # calls from id-name
-  document.querySelector(".first-name").innerText = contact.firstName;
-  document.querySelector(".last-name").innerText = contact.lastName;
-  document.querySelector(".phone-number").innerText = contact.phoneNumber;
-  document.querySelector(".email-address").innerText = contact.emailAddress;
-  document.querySelector(".home-address").innerText = contact.homeAddress;
-  document.querySelector("div#contact-details").removeAttribute("class");
-  // console.log("The id of this <li> is " + event.target.id + "."); --this console.log was the first line of code we added to this function from the lesson plan
-
-  // The line below this one is new to help with "delete functionality". our button will need access to the id of the current contact that's being displayed. The displayContactDetails() function already has access to the Contact object's details, so let's update that function to also update our new delete button with an id attribute set to the id of the contact.
-  document.querySelector("button.delete").setAttribute("id", contact.id);
-  //we are accessing our delete button in the html and then giving it an id attribute (because in the HTML it is only assigned a class attribute)set to the Contact object's id set in the javascript. 
-  document.querySelector("div#contact-details").removeAttribute("class");
-  //our CSS class is "hidden" which we already created, and that's why the delete works, because it "hides" our info like it's supposed to.
-}
-
-
-function handleDelete(event) {
-  addressBook.deleteContact(event.target.id);//addressBook is a global variable, we can call on it from within the handleDelete() function. Remember that we're using the global addressBook variable as a mock database 
-  document.querySelector("button.delete").removeAttribute("id");//we remove the id attribute from the delete button, thereby resetting it.
-  document.querySelector("div#contact-details").setAttribute("class", "hidden");//we are hiding the contact details div once more.
-  listContacts(addressBook);//Finally, we call the listContacts function again to refresh the list of contacts, this time without the contact that we've deleted.
-}
+<form id="new-order">
+<div class="pizza-size">
+  <h3>Choose Size:</h3>
+  <p>*Required: Choose One*</p>
+  <input type="radio" name="size" value="regular" id="regular" checked>
+  <label for="regular">Regular 12" - $20</label><br>
+  <input type="radio" name="size" value="Large" id="large">
+  <label for="large">Large 18" - $28</label><br>
+  <div class="pizza-toppings">
+    <br>
+    <h3>Choose Toppings:</h3>
+    <p>*Select as many as you like*</p>
+    <input type="checkbox" name="topping" value="plain" id="plain" checked>
+    <label for="regular">Plain: sauce and cheese only</label><br>
+    <input type="checkbox" name="topping" value="pepperoni" id="pepperoni">
+    <label for="large">Add Vegan Pepperoni +$4</label><br>
+    <input type="checkbox" name="topping" value="pineapple" id="pineapple">
+    <label for="regular">Add Pineapple +$3</label><br>
+    <input type="checkbox" name="topping" value="jalapeno" id="jalapeno">
+    <label for="large">Add Jalapeno +$3</label>
+    <br>
+    <br>
+    <button type="submit" class="btn-primary">Place Order</button>
+  </div>
+</form>
+function getResults(event) {
+  let form = document.getElementById("new-order")
+  event.preventDefault();
+  const animal = parseInt(document.querySelector("input[name='animal']:checked").value);
+  const allergy = parseInt(document.querySelector("input[name='allergy']:checked").value);
+  const soup = parseInt(document.querySelector("input[name='soup']:checked").value);
+  const shoes = parseInt(document.querySelector("input[name='shoes']:checked").value);
+  const resultRange = animal + allergy + soup + shoes;
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const inputtedFirstName = document.querySelector("input#new-first-name").value;
+  const inputtedFirstName = parseInt(document.querySelector("input#new-first-name").value);
   const inputtedLastName = document.querySelector("input#new-last-name").value;
   const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
   const inputtedEmailAddress = document.querySelector("input#new-email-address").value;
