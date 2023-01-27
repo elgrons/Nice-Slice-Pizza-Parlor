16c54e782a1a145f00f033ac5762f80349df957e
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-27 15:57:27 -0800

    update Javascript UI.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,38 @@
 // BUSINESS LOGIC
 
-//Business Logic for NewOrder (maybe a global variable and delete)
-function NewOrder () {
-  this.pizza = {};
-  this.orderSummary = orderSummary;
-}
-
 // Business Logic for my Constructor: NiceSlice:
-function NiceSlice(pizzaSize, pizzaTopping) {
+function NiceSlice(pizzaSize) {
   this.pizzaSize = pizzaSize;
-  this.pizzaTopping = pizzaTopping;
-  this.orderSummary = 0;
+  this.pizzaTopping = [];
+  this.pizzaTotal = 0;
 }
 
-NiceSlice.prototype.addSizeCost = function() {
-  this.pizzaSize = 0;
-  if (this.pizzaSize === "regular") {
-    this.pizzaSize + 20;
-  } 
-    else if (this.pizzaSize === "large") {
-    this.pizzaSize + 28;
-  }
-  return this.pizzaSize;
+NiceSlice.prototype.pizzaOrder = function() {
+    this.pizzaTotal = (this.pizzaTopping + i) + this.pizzaSize;
 }
+
+NiceSlice.prototype.orderTotal = function() {
+
+}
+
+// NiceSlice.prototype.addSizeCost = function() {
+//   this.pizzaSize = 0;
+//   if (this.pizzaSize === "regular") {
+//     this.pizzaSize + 20;
+//   } 
+//     else if (this.pizzaSize === "large") {
+//     this.pizzaSize + 28;
+//   }
+//   return this.pizzaSize;
+// }
 
 // NiceSlice.prototype.addToppingCost = function() {
+//   this.pizzaTopping = 0;
+//   for (let i = 0; i< this.pizzas.length; i++) {
+//     this.total += this.pizzas[i].cost;
+//   }
+// }
+
 //   let toppings = 
 //   this.pizzaTopping = 0;
 //   let pepperoni = this.pizzaTopping +4;
@@ -43,7 +51,7 @@ NiceSlice.prototype.orderSummary = function() {
 
 // Business Logic for orderSummary:
 function orderSummary() {
-  let pizzaTotal = this.pizzaSize + this.pizzaTopping
+  let pizzaTotal = this.pizzaSize + this.pizzaTopping;
   return pizzaTotal;
 }
 
@@ -51,54 +59,26 @@ function orderSummary() {
 
 let niceSlice = new NiceSlice();
 
-function orderSummary(orderSummaryToDisplay) {
-  document.querySelector(".first-name").innerText = contact.firstName;
-  let pizzaTotalDiv = document.querySelector("div#pizzaTotal");
-  return 
-}
+// function orderSummary(orderSummaryToDisplay) {
+//   document.querySelector(".first-name").innerText = contact.firstName;
+//   let pizzaTotalDiv = document.querySelector("div#pizzaTotal");
+//   return 
+// }
 
-window.addEventListener("load", function() {
+// window.addEventListener("load", function() {
 
-})
-
-<form id="new-order">
-<div class="pizza-size">
-  <h3>Choose Size:</h3>
-  <p>*Required: Choose One*</p>
-  <input type="radio" name="size" value="regular" id="regular" checked>
-  <label for="regular">Regular 12" - $20</label><br>
-  <input type="radio" name="size" value="Large" id="large">
-  <label for="large">Large 18" - $28</label><br>
-  <div class="pizza-toppings">
-    <br>
-    <h3>Choose Toppings:</h3>
-    <p>*Select as many as you like*</p>
-    <input type="checkbox" name="topping" value="plain" id="plain" checked>
-    <label for="regular">Plain: sauce and cheese only</label><br>
-    <input type="checkbox" name="topping" value="pepperoni" id="pepperoni">
-    <label for="large">Add Vegan Pepperoni +$4</label><br>
-    <input type="checkbox" name="topping" value="pineapple" id="pineapple">
-    <label for="regular">Add Pineapple +$3</label><br>
-    <input type="checkbox" name="topping" value="jalapeno" id="jalapeno">
-    <label for="large">Add Jalapeno +$3</label>
-    <br>
-    <br>
-    <button type="submit" class="btn-primary">Place Order</button>
-  </div>
-</form>
-function getResults(event) {
-  let form = document.getElementById("new-order")
-  event.preventDefault();
-  const animal = parseInt(document.querySelector("input[name='animal']:checked").value);
-  const allergy = parseInt(document.querySelector("input[name='allergy']:checked").value);
-  const soup = parseInt(document.querySelector("input[name='soup']:checked").value);
-  const shoes = parseInt(document.querySelector("input[name='shoes']:checked").value);
-  const resultRange = animal + allergy + soup + shoes;
+// })
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const inputtedFirstName = parseInt(document.querySelector("input#new-first-name").value);
-  const inputtedLastName = document.querySelector("input#new-last-name").value;
+  const regular = parseInt(document.querySelector("input[id='regular']:checked").value);
+  const large = parseInt(document.querySelector("input[id='large']").value);
+  const plain = parseInt(document.querySelector("input[id='plain']").value);
+  const pepperoni = parseInt(document.querySelector("input[id='pepperoni']").value);
+  const pineapple = parseInt(document.querySelector("input[id='pineapple']").value);
+  const jalapeno = parseInt(document.querySelector("input[id='jalapeno']").value);
+
+
   const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
   const inputtedEmailAddress = document.querySelector("input#new-email-address").value;
   const inputtedAddress = document.querySelector("input#new-home-address").value;
@@ -114,14 +94,10 @@ function handleFormSubmission(event) {
 
   //code from the lines above are to empty out our form fields after submission.
 }
-
-
 //event listeners actually call the functions to make them happen (ex. handleFormSubmission and displayContactDetails)
 //function() is a stand-in for the specific function listed below, and "submit", "click" etc are the events tied to the specific functions
 window.addEventListener("load", function() { 
   document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
   // The line below this one is new and is related to our function displayContactDetails()
   document.querySelector("div#contacts").addEventListener("click", displayContactDetails); 
-  // The line below this one is new and made according to page 20 "delete functionality"
-  document.querySelector("button.delete").addEventListener("click", handleDelete);
 });
